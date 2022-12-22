16cac3c4e8ff30f3b527835c4df3e9410b17bb17
Amari Cross <amaricross77@gmail.com>
2022-12-21 20:49:01 -0500

    and image change

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className="hero-content text-center">
       <div className="md:w-[550px] max-w-md">
         <h1 className=" md:text-7xl font-bold text-white text-4xl">iPhone 13</h1>
-        <img src="http://get2.imglarger.com:8889/results/vDUOWqo0_2x.jpg" alt="ok" className=""/>
+        <img src="https://cdn.discordapp.com/attachments/961667446199156829/1055300793059791000/vDUOWqo0_2x.png" alt="ok" className=""/>
         <p className="py-6 text-left">The iPhone 13 has a 6.1-inch screen, and the iPhone 13 mini has a 5.4-inch screen. They both use Apple&apos;s Ceramic Shield cover glass, which adds improved drop protection.</p>
         <button className="btn text-sky-600 mr-5 normal-case w-32">Get Started</button>
         <button className="btn p-0 normal-case w-32 bg-gradient-to-r from-cyan-500 to-blue-500">Get Started</button>
